9dd542497001acd513cc041cf6c21f874eca01bf
MetaCapFin <advisor@metacap.finance>
2025-02-18 17:36:59 -0500

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Dashboard
+      </Typography>
+      {/* Add more components and content here */}
+    </Container>
+  );
+};
+
+export default Dashboard;
