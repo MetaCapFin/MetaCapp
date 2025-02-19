90cb4b751e5eace21460c06536d7e7fcfd1436ec
MetaCapFin <advisor@metacap.finance>
2025-02-19 13:08:27 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import './Header.css';
 
 const Header = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" className="header-gradient">
       <Toolbar>
         <img src="/LogoName.png" alt="Logo Name" style={{ height: '40px' }} />
       </Toolbar>
